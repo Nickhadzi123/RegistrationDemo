85be969c75925fafec6daef3ecfaa0e87489fa42
nickh <nickhadzi123@gmail.com>
2023-11-08 15:06:49 -0500

    added second screen confirming data is valid

--- src/main/java/com/example/registrationdemo/HelloApplication.java
+++ src/main/java/com/example/registrationdemo/HelloApplication.java
@@ -6,13 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 440, 380);
-        scene.getStylesheets().add(getClass().getResource("textField.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("textField.css")).toExternalForm());
         stage.setTitle("Registration Menu");
         stage.setScene(scene);
         stage.show();
