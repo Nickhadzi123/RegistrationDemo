13050eb1d96fed4b946d9e3cfbf4f32cf1ac0698
nickh <nickhadzi123@gmail.com>
2023-11-08 15:05:33 -0500

    added second screen confirming data is valid

--- src/main/java/com/example/registrationdemo/HelloController.java
+++ src/main/java/com/example/registrationdemo/HelloController.java
@@ -1,9 +1,15 @@
 package com.example.registrationdemo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -15,19 +21,6 @@ public class HelloController {
     @FXML
     private Label firstNameErrorLabel, lastNameErrorLabel, emailErrorLabel, dateOfBirthErrorLabel, zipCodeErrorLabel;
 
-    public void initialize() {
-        setValidationListeners();
-        validateFields();
-    }
-
-    private void setValidationListeners() {
-        addValidationListener(firstNameField, firstNameErrorLabel, "[a-zA-Z]{2,25}");
-        addValidationListener(lastNameField, lastNameErrorLabel, "[a-zA-Z]{2,25}");
-        addValidationListener(emailField, emailErrorLabel, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
-        addValidationListener(dateOfBirthField, dateOfBirthErrorLabel, "^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}");
-        addValidationListener(zipCodeField, zipCodeErrorLabel, "\\d{5}");
-    }
-
     private void addValidationListener(TextField field, Label errorLabel, String pattern) {
         field.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue.matches(pattern)) {
@@ -35,13 +28,21 @@ public class HelloController {
                 field.getStyleClass().remove("text-field-valid");
                 field.getStyleClass().add("text-field-invalid");
                 validateFields();
-            } else {
+            }
+            else {
                 errorLabel.setText("");
                 field.getStyleClass().remove("text-field-invalid");
                 field.getStyleClass().add("text-field-valid");
                 validateFields();
             }
         });
+    }
+    private void setValidationListeners() {
+        addValidationListener(firstNameField, firstNameErrorLabel, "[a-zA-Z]{2,25}");
+        addValidationListener(lastNameField, lastNameErrorLabel, "[a-zA-Z]{2,25}");
+        addValidationListener(emailField, emailErrorLabel, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        addValidationListener(dateOfBirthField, dateOfBirthErrorLabel, "^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}");
+        addValidationListener(zipCodeField, zipCodeErrorLabel, "\\d{5}");
     }
 
     private void validateFields() {
@@ -53,4 +54,32 @@ public class HelloController {
 
         addbtn.setDisable(!(isFirstNameValid && isLastNameValid && isEmailValid && isDobValid && isZipCodeValid));
     }
+    private boolean isFormValid() {
+        return firstNameField.getStyleClass().contains("text-field-valid") &&
+                lastNameField.getStyleClass().contains("text-field-valid") &&
+                emailField.getStyleClass().contains("text-field-valid") &&
+                dateOfBirthField.getStyleClass().contains("text-field-valid") &&
+                zipCodeField.getStyleClass().contains("text-field-valid");
+    }
+
+    @FXML
+    private void initialize() {
+        setValidationListeners();
+        validateFields();
+
+    addbtn.setOnAction(event -> {
+        if (isFormValid()) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("nextView.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) addbtn.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace(); // Handle the exception as needed
+            }
+        }
+    });
+}
 }
---
+++ src/main/java/com/example/registrationdemo/NextController.java
@@ -0,0 +1,5 @@
+package com.example.registrationdemo;
+
+public class NextController {
+
+}
