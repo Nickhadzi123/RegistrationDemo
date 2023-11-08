bd122b7ef13bba399ca1d233ac7308acfb5bdd44
nickh <nickhadzi123@gmail.com>
2023-11-08 11:10:10 -0500

    fixed some of the labels

--- src/main/java/com/example/registrationdemo/HelloController.java
+++ src/main/java/com/example/registrationdemo/HelloController.java
@@ -6,89 +6,51 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-@FXML
-private TextField firstNameField;
-@FXML
-private TextField lastNameField;
-@FXML
-private TextField emailField;
-@FXML
-private TextField dateOfBirthField;
-@FXML
-private TextField zipCodeField;
-@FXML
-private Button addbtn;
-@FXML
-private Label errorLabel;
+    @FXML
+    private TextField firstNameField, lastNameField, emailField, dateOfBirthField, zipCodeField;
 
+    @FXML
+    private Button addbtn;
+
+    @FXML
+    private Label firstNameErrorLabel, lastNameErrorLabel, emailErrorLabel, dateOfBirthErrorLabel, zipCodeErrorLabel;
 
     public void initialize() {
+        setValidationListeners();
         validateFields();
-
-
-            firstNameField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("[a-zA-Z]{2,25}")) {
-                    errorLabel.setText("Invalid First Name");
-                    firstNameField.requestFocus();
-                } else {
-                    errorLabel.setText("");
-                    validateFields();
-                }
-            });
-
-            lastNameField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("[a-zA-Z]{2,25}")) {
-                    errorLabel.setText("Invalid Last Name");
-                    lastNameField.requestFocus();
-                } else {
-                    errorLabel.setText("");
-                    validateFields();
-                }
-            });
-
-            emailField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("[a-zA-Z0-9._%+-]+@farmingdale\\.com")) {
-                    errorLabel.setText("Invalid email, format must be @farmingdale.com");
-                    emailField.requestFocus();
-                } else {
-                    errorLabel.setText("");
-                    validateFields();
-                }
-            });
-
-            dateOfBirthField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}$")) {
-                    errorLabel.setText("Invalid DOB, must be ##/##/####");
-                    dateOfBirthField.requestFocus();
-                } else {
-                    errorLabel.setText("");
-                    validateFields();
-                }
-            });
-
-            zipCodeField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("\\d{5}")) {
-                    errorLabel.setText("Invalid zip code, must be 5 digits");
-                    zipCodeField.requestFocus();
-                } else {
-                    errorLabel.setText("");
-                    validateFields();
-                }
-            });
-        }
-
-        private void validateFields() {
-            boolean isFirstNameValid = firstNameField.getText().matches("[a-zA-Z]{2,25}");
-            boolean isLastNameValid = lastNameField.getText().matches("[a-zA-Z]{2,25}");
-            boolean isEmailValid = emailField.getText().matches("[a-zA-Z0-9._%+-]+@farmingdale\\.com");
-            boolean isDobValid = dateOfBirthField.getText().matches("^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}$");
-            boolean isZipCodeValid = zipCodeField.getText().matches("\\d{5}");
-
-            boolean allFieldsValid = isFirstNameValid && isLastNameValid && isEmailValid && isDobValid && isZipCodeValid;
-
-
-                addbtn.setDisable(!allFieldsValid);
-
-
-        }
     }
+
+    private void setValidationListeners() {
+        addValidationListener(firstNameField, firstNameErrorLabel, "[a-zA-Z]{2,25}");
+        addValidationListener(lastNameField, lastNameErrorLabel, "[a-zA-Z]{2,25}");
+        addValidationListener(emailField, emailErrorLabel, "[a-zA-Z0-9._%+-]+@farmingdale\\.com");
+        addValidationListener(dateOfBirthField, dateOfBirthErrorLabel, "^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}");
+        addValidationListener(zipCodeField, zipCodeErrorLabel, "\\d{5}");
+    }
+
+    private void addValidationListener(TextField field, Label errorLabel, String pattern) {
+        field.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches(pattern)) {
+                errorLabel.setText("Invalid input");
+                field.getStyleClass().remove("text-field-valid");
+                field.getStyleClass().add("text-field-invalid");
+                validateFields();
+            } else {
+                errorLabel.setText("");
+                field.getStyleClass().remove("text-field-invalid");
+                field.getStyleClass().add("text-field-valid");
+                validateFields();
+            }
+        });
+    }
+
+    private void validateFields() {
+        boolean isFirstNameValid = firstNameField.getText().matches("[a-zA-Z]{2,25}");
+        boolean isLastNameValid = lastNameField.getText().matches("[a-zA-Z]{2,25}");
+        boolean isEmailValid = emailField.getText().matches("[a-zA-Z0-9._%+-]+@farmingdale\\.com");
+        boolean isDobValid = dateOfBirthField.getText().matches("^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\\d{4}$");
+        boolean isZipCodeValid = zipCodeField.getText().matches("\\d{5}");
+
+        addbtn.setDisable(!(isFirstNameValid && isLastNameValid && isEmailValid && isDobValid && isZipCodeValid));
+    }
+}
